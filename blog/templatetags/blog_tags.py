f0e0174aaa7209cf367816c4a95e6be0db543818
LLoylento4bIx4ervei <kil-stas@rambler.ru>
2024-03-03 16:23:45 +0300

    Add blog

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from ..models import Post
+from django.db.models import Count
+import markdown
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_post(count=5):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post':latest_post}
+
+
+@register.simple_tag
+def get_most_commented_post(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
---
+++ blog/views.py
@@ -0,0 +1,94 @@
+from django.db.models import Count
+from django.shortcuts import render,get_object_or_404
+from .models import Post, Comments
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentsForm
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+from .forms import *
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)
+            ).filter(search=query)
+
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
+
+
+@require_POST
+def post_comments(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comments = None
+    form = CommentsForm(data=request.POST)
+    if form.is_valid():
+        comments = form.save(commit=False)
+        comments.post = post
+        comments.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comments})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data()
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
+
+
+def post_detail(request, year, month, day ,post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post, publish__year=year,
+                             publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentsForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', {'post': post, 'form': form, 'comments': comments, 'similar_posts':similar_posts})
+
+
+def view_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    post.view_cont += 1
+    post.save()
+    return render(request, 'blog/post/detail.html', context={'post': post})
